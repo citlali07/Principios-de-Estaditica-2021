1ada94108382627bc21ce0abab6bdfdfe331f334
citlali07 <78548842+citlali07@users.noreply.github.com>
2021-05-12 10:51:12 -0500

    Add files via upload

---
+++ Curso de Estadística Problema 1.R
@@ -0,0 +1,13 @@
+Pinus <- 3140
+Mezquite <- 1453
+Encinos <-450
+Teka <-1200
+Juiperos <- 720
+superficie <- c(Pinus, Mezquite, Encinos, Teka, Juiperos)
+barplot(superficie)
+help(sort)
+sort (superficie, decreasing = FALSE )
+sort (superficie, decreasing = TRUE )
+
+      
+
